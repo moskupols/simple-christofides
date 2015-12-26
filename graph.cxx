d4a4955cdc9e3b57a635b9de862a4b32b3f3ab72
Feodor Alexeev <feodor.alexeev@gmail.com>
2015-12-26 03:09:32 +0300

    Split graph into header and source

---
+++ graph.cxx
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+#include "graph.hxx"
+#include "dsu.hxx"
+
+using namespace std;
+
+vector<Edge> Graph::findMst() const
+{
+    vector<Edge> sortedEdges = edgeList;
+    sort(sortedEdges.begin(), sortedEdges.end(),
+            [](Edge a, Edge b) {return a.w < b.w; });
+
+    vector<Edge> ret;
+    ret.reserve(n-1);
+
+    DisjointSetUnion dsu(n);
+    for (Edge e : sortedEdges)
+        if (dsu.join(e.from, e.to))
+            ret.push_back(e);
+
+    return ret;
+}
+
+vector<Edge> Graph::findOptimalMatchingApprox(
+        unordered_set<int> ignoredVertices) const
+{
+    vector<Edge> sortedEdges = edgeList;
+    sort(sortedEdges.begin(), sortedEdges.end(),
+            [](Edge a, Edge b) {return a.w < b.w; });
+
+    vector<Edge> ret;
+    for (Edge e : sortedEdges)
+        if (ignoredVertices.count(e.from) == 0 && ignoredVertices.count(e.to) == 0)
+        {
+            ignoredVertices.insert(e.from);
+            ignoredVertices.insert(e.to);
+            ret.push_back(e);
+        }
+    return ret;
+}
+
+vector<vector<Edge>> Graph::getAdjList() const
+{
+    vector<vector<Edge>> ret(n);
+    for (Edge e : edgeList)
+    {
+        ret[e.from].push_back(e);
+        ret[e.to].push_back(Edge{e.id, e.to, e.from, e.w});
+    }
+    return ret;
+}
+
+vector<int> Graph::buildEulerianTourWithShortcuts(int start)
+{
+    vector<int> tour = buildEulerianTour(start);
+    vector<int> ret;
+    unordered_set<int> met;
+    for (int x : tour)
+        if (met.insert(x).second)
+            ret.push_back(x);
+    return ret;
+}
+
+namespace
+{
+void buildEulerianTourRecursive(int v, vector<int> &ret,
+        vector<vector<Edge>> &adjList, unordered_set<int> &edgeDeleted)
+{
+    while (!adjList[v].empty())
+    {
+        Edge e = adjList[v].back();
+        adjList[v].pop_back();
+        if (edgeDeleted.insert(e.id).second)
+            buildEulerianTourRecursive(e.to, ret, adjList, edgeDeleted);
+    }
+    ret.push_back(v);
+}
+}
+
+vector<int> Graph::buildEulerianTour(int start)
+{
+    vector<int> ret;
+    vector<vector<Edge>> adjList = getAdjList();
+    unordered_set<int> edgeDeleted;
+    buildEulerianTourRecursive(start, ret, adjList, edgeDeleted);
+    return ret;
+}
+
+namespace
+{
+static Edge buildEdge(const vector<Point> &p, int a, int b)
+{
+    return {a * (int)p.size() + b, a, b, Point(p[a], p[b]).radius()};
+}
+
+static vector<Edge> buildEdges(const vector<Point> &p)
+{
+    vector<Edge> ret;
+    for (size_t i = 0; i < p.size(); ++i)
+        for (size_t j = i + 1; j < p.size(); ++j)
+            ret.push_back(buildEdge(p, i, j));
+    return ret;
+}
+}
+
+EuclideanGraph::EuclideanGraph(vector<Point> p):
+    Graph(p.size(), buildEdges(p)),
+    p(p)
+{}
+
+Edge EuclideanGraph::getEdge(int a, int b) const
+{
+    return {a * (int)p.size() + b, a, b, Point(p[a], p[b]).radius()};
+}
+
--- graph.hxx
+++ graph.hxx
@@ -1,8 +1,9 @@
 #ifndef GRAPH_HXX
 #define GRAPH_HXX
 
-#include "dsu.hxx"
-using namespace std;
+#include <vector>
+#include <unordered_set>
+#include <cmath>
 
 struct Point
 {
@@ -11,10 +12,7 @@ struct Point
     Point(double x, double y): x(x), y(y) {}
     Point(Point a, Point b): x(b.x-a.x), y(b.y-a.y) {}
 
-    double radius() const
-    {
-        return sqrt(x*x + y*y);
-    }
+    double radius() const { return sqrt(x*x + y*y); }
 };
 
 struct Edge
@@ -29,130 +27,39 @@ struct Graph
         n(n)
     {}
 
-    explicit Graph(int n, vector<Edge> edgeList):
-        Graph::Graph(n)
-    {
-        this->edgeList.reserve(edgeList.size());
-        for (Edge e : edgeList)
-            addEdge(e);
-    }
+    explicit Graph(int n, std::vector<Edge> edgeList):
+        n(n),
+        edgeList(edgeList)
+    {}
 
     void addEdge(Edge e)
     {
         edgeList.push_back(e);
     }
 
-    vector<Edge> findMst() const
-    {
-        vector<Edge> sortedEdges = edgeList;
-        sort(sortedEdges.begin(), sortedEdges.end(),
-                [](Edge a, Edge b) {return a.w < b.w; });
+    std::vector<Edge> findMst() const;
 
-        vector<Edge> ret;
-        ret.reserve(n-1);
+    std::vector<Edge> findOptimalMatchingApprox(
+            std::unordered_set<int> ignoredVertices = {}) const;
 
-        DisjointSetUnion dsu(n);
-        for (Edge e : sortedEdges)
-            if (dsu.join(e.from, e.to))
-                ret.push_back(e);
+    std::vector<std::vector<Edge>> getAdjList() const;
 
-        return ret;
-    }
-
-    vector<Edge> findOptimalMatchingApprox(std::unordered_set<int> ignoredVertices = {}) const
-    {
-        vector<Edge> sortedEdges = edgeList;
-        sort(sortedEdges.begin(), sortedEdges.end(),
-                [](Edge a, Edge b) {return a.w < b.w; });
-
-        vector<Edge> ret;
-        for (Edge e : sortedEdges)
-            if (ignoredVertices.count(e.from) == 0 && ignoredVertices.count(e.to) == 0)
-            {
-                ignoredVertices.insert(e.from);
-                ignoredVertices.insert(e.to);
-                ret.push_back(e);
-            }
-        return ret;
-    }
-
-    vector<vector<Edge>> getAdjList() const
-    {
-        vector<vector<Edge>> ret(n);
-        for (Edge e : edgeList)
-        {
-            ret[e.from].push_back(e);
-            ret[e.to].push_back(Edge{e.id, e.to, e.from, e.w});
-        }
-        return ret;
-    }
-
-    vector<int> buildEulerianTourWithShortcuts(int start = 0)
-    {
-        vector<int> tour = buildEulerianTour(start);
-        vector<int> ret;
-        unordered_set<int> met;
-        for (int x : tour)
-            if (met.insert(x).second)
-                ret.push_back(x);
-        return ret;
-    }
-
-    vector<int> buildEulerianTour(int start = 0)
-    {
-        vector<int> ret;
-        vector<vector<Edge>> adjList = getAdjList();
-        unordered_set<int> edgeDeleted;
-        buildEulerianTourRecursive(start, ret, adjList, edgeDeleted);
-        return ret;
-    }
+    std::vector<int> buildEulerianTourWithShortcuts(int start = 0);
+    std::vector<int> buildEulerianTour(int start = 0);
 
     int n;
-    vector<Edge> edgeList;
+    std::vector<Edge> edgeList;
 
 private:
-    static void buildEulerianTourRecursive(int v, vector<int> &ret,
-            vector<vector<Edge>> &adjList, unordered_set<int> &edgeDeleted)
-    {
-        while (!adjList[v].empty())
-        {
-            Edge e = adjList[v].back();
-            adjList[v].pop_back();
-            if (edgeDeleted.insert(e.id).second)
-                buildEulerianTourRecursive(e.to, ret, adjList, edgeDeleted);
-        }
-        ret.push_back(v);
-    }
 };
 
 struct EuclideanGraph : public Graph
 {
-    explicit EuclideanGraph(vector<Point> p):
-        Graph(p.size(), buildEdges(p)),
-        p(p)
-    {}
+    explicit EuclideanGraph(std::vector<Point> p);
 
-    Edge getEdge(int a, int b) const
-    {
-        return {a * (int)p.size() + b, a, b, Point(p[a], p[b]).radius()};
-    }
+    Edge getEdge(int a, int b) const;
 
-    vector<Point> p;
-
-protected:
-    static Edge buildEdge(const vector<Point> &p, int a, int b)
-    {
-        return {a * (int)p.size() + b, a, b, Point(p[a], p[b]).radius()};
-    }
-
-    static vector<Edge> buildEdges(const vector<Point> &p)
-    {
-        vector<Edge> ret;
-        for (size_t i = 0; i < p.size(); ++i)
-            for (size_t j = i + 1; j < p.size(); ++j)
-                ret.push_back(buildEdge(p, i, j));
-        return ret;
-    }
+    std::vector<Point> p;
 };
 
 #endif
